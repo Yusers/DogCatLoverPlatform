ba6381042e390e74e756cbaed2d4c014669c0dd0
Yusers <tanyusisreal@gmail.com>
2023-10-16 18:03:17 +0700

    upload src

---
+++ src/java/controllers/DispatcherController.java
@@ -0,0 +1,96 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controllers;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ *
+ * @author Kuuga
+ */
+public class DispatcherController extends HttpServlet {
+
+    final String HOME_PAGE = "index.jsp";
+    final String ADMIN_PAGE = "adminpage.jsp";
+    final String LOGIN_PAGE = "login.jsp";
+    final String REGISTER_PAGE = "signup.jsp";
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            String url = HOME_PAGE;
+            String action = request.getParameter("action");
+            if (action == null || action.isEmpty()) {
+                url = HOME_PAGE;
+            } else if(action.equals("login")) {
+                url = "LoginController";
+            } else if(action.equals("login-page")) {
+                url = LOGIN_PAGE;
+            } else if(action.equals("logout")) {
+                url = "LogoutController";
+            } else if(action.equals("register")) {
+                url = "RegisterController";
+            } else if(action.equals("register-page")) {
+                url = REGISTER_PAGE;
+            }
+            // Chuyển hướng đến trang tương ứng với chức năng đã chọn
+            request.getRequestDispatcher(url).forward(request, response);
+        }
+    }
+
+// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ src/java/dbaccess/AccountDAO.java
@@ -0,0 +1,224 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dbaccess;
+
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.util.Date;
+import model.Account;
+import myutils.DBUtils;
+
+/**
+ *
+ * @author Kuuga
+ */
+public class AccountDAO {
+
+    public static Account checkLogin(String id, String password) throws Exception {
+        Connection cn = DBUtils.makeConnection();
+        Account account = null;
+        if (cn != null) {
+            //b2: viet sql va exec
+            String sql = "SELECT [user_id],[fullname],[email],[password],[phone_number],[description],[role],[status],[created_at],[updated_at]\n"
+                    + "FROM [dbo].[Account]\n"
+                    + "WHERE [user_id] LIKE ? AND [password] LIKE ?";
+            PreparedStatement pst = cn.prepareStatement(sql);
+            pst.setString(1, id);
+            pst.setString(2, password);
+            ResultSet rs = pst.executeQuery();
+            if (rs != null) {
+                while (rs.next()) {
+                    String user_id = rs.getString("user_id");
+                    String name = rs.getString("fullname");
+                    String email = rs.getString("email");
+                    String pass = rs.getString("password");
+                    String phonenumber = rs.getString("phone_number");
+                    String des = rs.getString("description");
+                    String roleID = rs.getString("role");
+                    String status = rs.getString("status");
+                    Date createdat = rs.getDate("created_at");
+                    Date updatedat = rs.getDate("updated_at");
+                    account = new Account(user_id, name, email, pass, phonenumber, des, roleID, status, createdat, updatedat);
+                }
+            }
+            cn.close();
+        }
+        return account;
+    }
+
+    public static int createAccount(Account acc) throws Exception {
+        int rs = 0;
+        Connection cn = DBUtils.makeConnection();
+        if (cn != null) {
+            String sql = "INSERT INTO Account ( user_id,fullname, email, password, phone_number, description, role, status)\n"
+                    + "VALUES \n"
+                    + "    ( ?, ?, ?, ?, ?, ?, ?, ?);";
+            PreparedStatement pst = cn.prepareStatement(sql);
+            pst.setString(1, acc.getUser_id());
+            pst.setString(2, acc.getFullname());
+            pst.setString(3, acc.getEmail());
+            pst.setString(4, acc.getPassword());
+            pst.setString(5, acc.getPhone_number());
+            pst.setString(6, "do not have bio yet");
+            pst.setString(7, "MEMBER");
+            pst.setString(8, "active");
+            rs = pst.executeUpdate();
+            cn.close();
+        }
+
+        return rs;
+    }
+
+    public static ArrayList<Account> getAllMember() throws Exception {
+        Connection cn = DBUtils.makeConnection();
+        ArrayList<Account> list = new ArrayList<>();
+        if (cn != null) {
+            String sql = "SELECT [user_id],[fullname],[email],[phone_number],[description],[role],[status],[created_at]\n"
+                    + "FROM [dbo].[Account]\n"
+                    + "WHERE [role] LIKE 'MEMBER'";
+            Statement st = cn.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            if (rs != null) {
+                while (rs.next()) {
+                    String id = rs.getString("user_id");
+                    String name = rs.getString("fullname");
+                    String email = rs.getString("email");
+                    String phone_number = rs.getString("phone_number");
+                    String des = rs.getString("description");
+                    String role = rs.getString("role");
+                    String status = rs.getString("status");
+                    Date createdat = rs.getDate("created_at");
+                    Account account = new Account(id, name, email, phone_number, des, role, status, createdat);
+                    list.add(account);
+                }
+            }
+            cn.close();
+        }
+        return list;
+    }
+
+    public static ArrayList<Account> getAllStaff() throws Exception {
+        Connection cn = DBUtils.makeConnection();
+        ArrayList<Account> list = new ArrayList<>();
+        if (cn != null) {
+            String sql = "SELECT [user_id],[fullname],[email],[phone_number],[description],[role],[status],[created_at]\n"
+                    + "FROM [dbo].[Account]\n"
+                    + "WHERE [role] LIKE 'STAFF'";
+            Statement st = cn.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            if (rs != null) {
+                while (rs.next()) {
+                    String id = rs.getString("user_id");
+                    String name = rs.getString("fullname");
+                    String email = rs.getString("email");
+                    String phone_number = rs.getString("phone_number");
+                    String des = rs.getString("description");
+                    String role = rs.getString("role");
+                    String status = rs.getString("status");
+                    Date createdat = rs.getDate("created_at");
+                    Account account = new Account(id, name, email, phone_number, des, role, status, createdat);
+                    list.add(account);
+                }
+            }
+            cn.close();
+        }
+        return list;
+    }
+
+    public static Account getAccount(String userid) throws Exception {
+        Account account = null;
+        Connection cn = DBUtils.makeConnection();
+        if (cn != null) {
+            String sql = "SELECT *\n"
+                    + "FROM [dbo].[Account]\n"
+                    + "WHERE [user_id] LIKE ?";
+            PreparedStatement pst = cn.prepareStatement(sql);
+            pst.setString(1, userid);
+            ResultSet rs = pst.executeQuery();
+            if (rs != null) {
+                while (rs.next()) {
+                    String id = rs.getString("user_id");
+                    String name = rs.getString("fullname");
+                    String email = rs.getString("email");
+                    String pass = rs.getString("password");
+                    String phone_number = rs.getString("phone_number");
+                    String des = rs.getString("description");
+                    String role = rs.getString("role");
+                    String status = rs.getString("status");
+                    Date createdat = rs.getDate("created_at");
+                    Date updatedat = rs.getDate("updated_at");
+                    account = new Account(id, name, email, pass, phone_number, des, role, status, createdat, updatedat);
+                }
+            }
+            cn.close();
+        }
+        return account;
+    }
+
+    public static int banAccount(String username) throws Exception {
+        int rs = 0;
+        Connection cn = DBUtils.makeConnection();
+        if (cn != null) {
+            String sql = "UPDATE [dbo].[Account]\n"
+                    + "SET [status] = CASE\n"
+                    + "    WHEN [status] LIKE 'ACTIVE' THEN 'INACTIVE'\n"
+                    + "    ELSE 'ACTIVE'\n"
+                    + "END\n"
+                    + "WHERE [user_id] LIKE ?";
+            PreparedStatement pst = cn.prepareStatement(sql);
+            pst.setString(1, username);
+            rs = pst.executeUpdate();
+            cn.close();
+        }
+        return rs;
+    }
+
+    public static int deleteAccount(String username) throws Exception {
+        int rs = 0;
+        Connection cn = DBUtils.makeConnection();
+        if (cn != null) {
+            String sql = "delete from [dbo].[Account] \n"
+                    + "where [user_id] LIKE ?";
+            PreparedStatement pst = cn.prepareStatement(sql);
+            pst.setString(1, username);
+            rs = pst.executeUpdate();
+            cn.close();
+        }
+        return rs;
+    }
+
+    public static ArrayList<Account> searchStaff(String search) throws Exception {
+        Connection cn = DBUtils.makeConnection();
+        ArrayList<Account> list = new ArrayList<>();
+        if (cn != null) {
+            String sql = "SELECT [user_id],[fullname],[email],[phone_number],[description],[role],[status],[created_at]\n"
+                    + "FROM [dbo].[Account]\n"
+                    + "WHERE [user_id] LIKE ? OR [fullname] LIKE ? AND [role] LIKE 'STAFF'";
+            PreparedStatement pst = cn.prepareStatement(sql);
+            pst.setString(1, search);
+            pst.setString(2, "%" + search + "%");
+            ResultSet rs = pst.executeQuery();
+            if (rs != null) {
+                while (rs.next()) {
+                    String id = rs.getString("user_id");
+                    String name = rs.getString("fullname");
+                    String email = rs.getString("email");
+                    String phone_number = rs.getString("phone_number");
+                    String des = rs.getString("description");
+                    String role = rs.getString("role");
+                    String status = rs.getString("status");
+                    Date createdat = rs.getDate("created_at");
+                    Account account = new Account(id, name, email, phone_number, des, role, status, createdat);
+                    list.add(account);
+                }
+            }
+            cn.close();
+        }
+        return list;
+    }
+}
